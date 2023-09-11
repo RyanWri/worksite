dfb6c6ed4b6179cd468e7a5fa21250c26ef434e3
RyanWri <ranman37@gmail.com>
2023-09-11 14:04:10 +0300

    mobile adjustment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,31 +3,16 @@
   import Footer from "./Footer.svelte";
   import "./styles.css";
   import "../app.css";
+  import Header2 from "./Header2.svelte";
 </script>
 
-<div class="app">
-  <Header />
-  <main>
-    <slot />
-  </main>
+<header>
+  <Header2 />
+</header>
+<main class="h-screen w-screen">
+  <slot />
+</main>
+
+<footer class="bg-gray-100 mt-1">
   <Footer />
-</div>
-
-<style>
-  .app {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-    min-width: 75vh;
-  }
-
-  main {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    padding: 1rem;
-    width: 100%;
-    margin: 0 auto;
-    box-sizing: border-box;
-  }
-</style>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,17 @@
-<svelte:head>
-  <title>Home</title>
-  <meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section
-  class="w-full aspect-w-1 aspect-h-1 bg-cover bg-center"
-  style="background-image: url('/images/welcome-bg-comp.jpg')"
->
-  <h1 class="text-2xl font-bold capitalize">Welcome to my website</h1>
-  <p class="pt-6 text-xl mb-2 animated-text">
-    I am here to assist you with the navigation
-  </p>
-  <a href="/about" class="text-blue-500 mb-2 animated-paragraph">About</a>
-  <a href="/projects" class="text-blue-500 mb-2 animated-paragraph">Projects</a>
-</section>
-
-<style>
-  section {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    flex: 0.9;
-  }
-</style>
+<div class="flex h-screen">
+  <div
+    class="flex-1 flex-col bg-cover"
+    style="background-image: url('/images/welcome-bg-comp.jpg')"
+  >
+    <h1 class="text-2xl font-bold capitalize text-center">
+      Welcome to my website
+    </h1>
+    <p class="pt-6 text-xl mb-2 animated-text">
+      I am here to assist you with the navigation
+    </p>
+    <a href="/about" class="text-blue-500 mb-2 animated-paragraph">About</a>
+    <a href="/projects" class="text-blue-500 mb-2 animated-paragraph"
+      >Projects</a
+    >
+  </div>
+</div>
