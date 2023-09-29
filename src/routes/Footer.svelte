f52bd4582a20ab63921d9f9d2a5136a5f7efd5f3
RyanWri <ranman37@gmail.com>
2023-09-29 13:38:22 +0300

    seo and footer links

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -5,10 +5,14 @@
 <footer class="bg-gray-200">
   <div class="flex justify-between w-1/3 py-2">
     <!-- Social icons -->
-    <a href="#id"><Icon icon="mdi:github" /></a>
-    <a href="#id"><Icon icon="mdi:linkedin" /></a>
-    <a href="#id"><Icon icon="mdi:facebook" /></a>
-    <a href="#id"><Icon icon="mdi:instagram" /></a>
-    <a href="#id"><Icon icon="mdi:discord" /></a>
+    <a href="https://github.com/RyanWri"><Icon icon="mdi:github" /></a>
+    <a href="https://www.linkedin.com/in/ran-wright46/"
+      ><Icon icon="mdi:linkedin" /></a
+    >
+    <a href="https://www.facebook.com/rrwright46/"
+      ><Icon icon="mdi:facebook" /></a
+    >
+    <!-- <a href="fill your instagram link"><Icon icon="mdi:instagram" /></a>
+    <a href="fill your discord link"><Icon icon="mdi:discord" /></a> -->
   </div>
 </footer>
