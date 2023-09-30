43e231f95437fb1c7d2512abc3c08545245c9100
RyanWri <ranman37@gmail.com>
2023-09-30 12:09:47 +0300

    footer with valid links and trademark

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -2,17 +2,38 @@
   import Icon from "@iconify/svelte";
 </script>
 
-<footer class="bg-gray-200">
-  <div class="flex justify-between w-1/3 py-2">
-    <!-- Social icons -->
-    <a href="https://github.com/RyanWri"><Icon icon="mdi:github" /></a>
-    <a href="https://www.linkedin.com/in/ran-wright46/"
-      ><Icon icon="mdi:linkedin" /></a
-    >
-    <a href="https://www.facebook.com/rrwright46/"
-      ><Icon icon="mdi:facebook" /></a
-    >
-    <!-- <a href="fill your instagram link"><Icon icon="mdi:instagram" /></a>
-    <a href="fill your discord link"><Icon icon="mdi:discord" /></a> -->
+<footer class="dark-header">
+  <hr class="my-3 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-4" />
+  <div class="sm:flex sm:items-center sm:justify-between">
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
+      >© 2023 <a href="/" class="hover:underline">The Wright Way™</a>. All
+      Rights Reserved.
+    </span>
+    <div class="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
+      <a
+        href="https://github.com/RyanWri"
+        class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+      >
+        <Icon icon="mdi:github" />
+      </a>
+      <a
+        href="https://discordapp.com/users/1119880721289453639"
+        class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+      >
+        <Icon icon="mdi:discord" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/ran-wright46/"
+        class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+      >
+        <Icon icon="mdi:linkedin" />
+      </a>
+      <a
+        href="https://www.facebook.com/rrwright46/"
+        class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+      >
+        <Icon icon="mdi:facebook" />
+      </a>
+    </div>
   </div>
 </footer>
