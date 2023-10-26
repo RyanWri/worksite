663ac00e8ea3135cd39ce4722c25ef567b726399
RyanWri <ranman37@gmail.com>
2023-10-26 10:16:54 +0300

    using SSR and adding Get endpoint

---
+++ src/routes/skills/+page.server.js
@@ -0,0 +1,10 @@
+import fs from "fs";
+
+export async function load() {
+  const jsonData = fs.readFileSync("src/lib/data/skills.json", "utf-8");
+  const skills = JSON.parse(jsonData);
+
+  return {
+    skills,
+  };
+}
