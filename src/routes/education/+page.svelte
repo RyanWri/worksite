dbe07eaf184fced7b70dd06ef508a297b149079d
RyanWri <ranman37@gmail.com>
2024-06-09 17:44:03 +0300

    updating info and icons

--- src/routes/education/+page.svelte
+++ src/routes/education/+page.svelte
@@ -6,7 +6,7 @@
 <div class="container mx-auto">
   <h1 class="page-title">Education</h1>
   <ol class="relative border-l border-gray-200 mt-3">
-    {#each timelineData.reverse() as item}
+    {#each timelineData as item}
       <li class="mb-10 ml-4">
         <div
           class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
