9fb8c09e59f4a169b34ff81fe5ff02cd2f59031b
RyanWri <ranman37@gmail.com>
2024-06-23 16:05:22 +0300

    new projects page

---
+++ src/lib/components/Project.svelte
@@ -0,0 +1,17 @@
+<!-- Project.svelte -->
+<script>
+  import Icon from "@iconify/svelte";
+
+  export let project;
+</script>
+
+<div class="border border-r-2 p-4 mt-4 mb-4">
+  <h2 class="text-lg font-bold">{project.company}</h2>
+  <p><strong>Purpose:</strong> {project.purpose}</p>
+  <p><strong>Technologies:</strong></p>
+  <div class="flex flex-wrap">
+    {#each project.technologies as tech}
+      <Icon icon={tech} height="48" width="48" />
+    {/each}
+  </div>
+</div>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Project from "$lib/components/Project.svelte";
+  import { onMount } from "svelte";
+
+  let projects = [];
+  onMount(async () => {
+    const response = await fetch("/data/projects.json");
+    if (response.ok) {
+      projects = await response.json();
+    } else {
+      console.error("Failed to fetch projects");
+    }
+  });
+</script>
+
+<div class="grid lg:grid-cols-2 md:grid-cols-3 gap-4">
+  {#each projects as project}
+    <Project {project} />
+  {/each}
+</div>
